bce7ff167d5c17815cc46617c1b9e0eb4242ca3e
Golam Rahman Sagor <grsagor08@gmail.com>
2023-05-10 01:58:43 +0600

    creating and joining class done

--- index.js
+++ index.js
@@ -7,16 +7,6 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-async function generateRandomValue() {
-  const randomValue = shortid.generate({ length: 6 });
-  const document = await collection.findOne({ value: randomValue });
-  if (document) {
-    return generateRandomValue();
-  } else {
-    return randomValue;
-  }
-}
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -65,6 +55,22 @@ async function run() {
   try {
     const usersCollection = client.db('LMS').collection('users');
     const postCollection = client.db('LMS').collection('posts');
+    const classCollection = client.db('LMS').collection('classes');
+
+    async function generateRandomValue() {
+      const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+      const randomValue9 = shortid.generate({ 
+        length: 6,
+        characters: characters
+      });
+      const randomValue = randomValue9.slice(0,6);
+      const document = await classCollection.findOne({ code: randomValue });
+      if (document) {
+        return generateRandomValue();
+      } else {
+        return randomValue;
+      }
+    }
 
     app.get('/users', async (req,res) => {
         let query = {};
@@ -99,11 +105,59 @@ async function run() {
           };
           const options = { w: "majority" };
           postCollection.insertOne(post, options, (err, result) => {
-
           });
         }
       });
     });
+
+    app.post('/create-class', async (req, res) => {
+      const body = req.body;
+      const options = {upsert: true};
+
+      body.code = await generateRandomValue();
+      const result = await classCollection.insertOne(body);
+
+      const document = await classCollection.findOne({ code: body.code });
+      const email = body?.teacher;
+      const filterUser = {email: email};
+      const documentUser = await usersCollection.findOne(filterUser);
+      const updateDoc ={
+          $set:{
+            classes: [document?._id,...documentUser?.classes]
+          }
+      }
+      const resultUser = await usersCollection.updateOne(filterUser, updateDoc, options);
+      res.send(resultUser);
+    });
+
+    app.put('/join-class', async(req, res)=> {
+      const body = req.body;
+      const options = {upsert: true};
+      const code = body.code;
+
+      const filter = {code: code};
+      const document = await classCollection.findOne({ code: code });
+      console.log(document);
+      const updateDocClass ={
+          $set:{
+            students: [body?.email,...document?.students]
+          }
+      }
+      const result = await classCollection.updateOne(filter, updateDocClass, options);
+
+      const email = body?.email;
+      const filterUser = {email: email};
+      const documentUser = await usersCollection.findOne(filterUser);
+      const updateDoc ={
+          $set:{
+            classes: [document?._id,...documentUser?.classes]
+          }
+      }
+      const resultUser = await usersCollection.updateOne(filterUser, updateDoc, options);
+
+        res.send(resultUser);
+
+  })
 
     app.get('/', (req,res) => {
         res.send('LMS is runninig');
